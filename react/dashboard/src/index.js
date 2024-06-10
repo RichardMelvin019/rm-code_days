c9810f58f56755cc1ca9ac8eb9dc3a31fcea8645
RichardMelvin019 <richardmelvin019@gmail.com>
2024-06-11 00:47:20 +0100

    Notifications added

--- react/dashboard/src/index.js
+++ react/dashboard/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Notifications from "./Notifications";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <div className="root-notifications">
+      <Notifications />
+    </div>
   </React.StrictMode>
 );
 
