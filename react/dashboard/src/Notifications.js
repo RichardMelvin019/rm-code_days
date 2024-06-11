eb36dc34ae919fb4006480221c8efdd0a165e639
RichardMelvin019 <richardmelvin019@gmail.com>
2024-06-11 23:21:41 +0100

    notifications created successfully

--- react/dashboard/src/Notifications.js
+++ react/dashboard/src/Notifications.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import './Notifications.css';
+import React from "react";
+import "./Notifications.css";
+import closeIcon  from "./close-icon.png";
+import { getLatestNotification } from "./utils.js";
 
 function Notifications() {
   return (
-    <div className='Notifications'>
+    <div className="Notifications">
       <p>Here is the list of notifications</p>
+      <button
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "10px",
+          background: "none",
+          border: "none",
+          cursor: "pointer",
+        }}
+        aria-label="close"
+        onClick={console.log("Close button has been clicked")}
+      >
+        <img
+          style={{
+            display: "inline",
+            marginTop: "10px",
+            marginRight: "10px",
+            padding: "0",
+          }}
+          src={closeIcon}
+          alt="Close"
+          width="15px"
+        />
+      </button>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        
+      </ul>
     </div>
   );
 }
